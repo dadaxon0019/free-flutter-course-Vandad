d9758c0197a7fa163c95bab044ebe9381a5d53bb
Dadahon0019 <95474203+dadaxon0019@users.noreply.github.com>
2023-04-26 19:17:58 +0300

    email verificaton is done

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:free_flutter_course/firebase_options.dart';
 import 'package:free_flutter_course/view/login_view.dart';
+import 'package:free_flutter_course/view/regist_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,16 +34,41 @@ class HomePage extends StatelessWidget {
             case ConnectionState.done:
               final user = FirebaseAuth.instance.currentUser;
               if (user?.emailVerified ?? false) {
-                print('You are a verified user');
+                print(user);
+                return const Text('Done');
               } else {
-                print('You need to verify your email first');
+                return const VerifyEmailView();
               }
-              return Text('Done');
             default:
               return const Text('Login');
           }
         },
       ),
+    );
+  }
+}
+
+class VerifyEmailView extends StatefulWidget {
+  const VerifyEmailView({super.key});
+
+  @override
+  State<VerifyEmailView> createState() => _VerifyEmailViewState();
+}
+
+class _VerifyEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text('Please verify your email'),
+        TextButton(
+          onPressed: () async {
+            final user = FirebaseAuth.instance.currentUser;
+            await user?.sendEmailVerification();
+          },
+          child: const Text('Send email verification'),
+        ),
+      ],
     );
   }
 }
